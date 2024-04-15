9a411f9342d4441f852f8d8a0676cc73c80e111a
whettam <mjballard88@gmail.com>
2024-04-15 08:38:44 -0500

    Initial Commit

---
+++ API_Lab/API_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_LabApp.swift
+//  API_Lab
+//
+//  Created by BALLARD, MATTHEW J. on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct API_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API_Lab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API_Lab
+//
+//  Created by BALLARD, MATTHEW J. on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
